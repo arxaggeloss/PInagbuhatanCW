c4dcea15e0ae136c7afe7e4d07a6387d1f511c35
arxaggeloss <151056102+arxaggeloss@users.noreply.github.com>
2024-05-28 01:51:44 +0800

    Update viewCategories.php

--- admin_panel/adminView/viewCategories.php
+++ admin_panel/adminView/viewCategories.php
@@ -11,13 +11,13 @@ require 'C:\xampp\htdocs\TANDAAN\PHPMailer-master\src\SMTP.php';
 $mail = new PHPMailer(true);
 
 // Database connection
-   $servername = "pinagbuhatancw.mysql.database.azure.com";
+$servername = "pinagbuhatancw.mysql.database.azure.com";
 $username_db = "pinagbuhatancw";
 $password_db = 'pa$$word1';
 $database = "tandaandb";
 
-// Establish connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Create a connection to the database
+$conn = new mysqli($servername, $username_db, $password_db, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
