af3650f470de0cf9864ae35017e8f66d5d315714
arxaggeloss <151056102+arxaggeloss@users.noreply.github.com>
2024-05-28 20:55:57 +0800

    Update edit_user.php

--- admin_panel/edit_user.php
+++ admin_panel/edit_user.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "pinagbuhatancw.mysql.database.azure.com";
-$username_db = "pinagbuhatancw";
+$servername = 'pinagbuhatancw.mysql.database.azure.com';
+$username_db = 'pinagbuhatancw';
 $password_db = 'pa$$word1';
-$database = "tandaandb";
+$database = 'tandaandb';
 
 // Create a connection to the database
 $conn = new mysqli($servername, $username_db, $password_db, $database);
