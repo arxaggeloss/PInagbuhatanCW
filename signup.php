a7399949587c99139ad85c08ebcc38da7f5b2ccd
arxaggeloss <151056102+arxaggeloss@users.noreply.github.com>
2024-05-28 14:10:59 +0800

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,9 +4,9 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-require 'PHPMailer-master\src\Exception.php';
-require 'PHPMailer-master\src\PHPMailer.php';
-require 'PHPMailer-master\src\SMTP.php';
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
 // Function to generate OTP
 function generateOTP($length = 6) {
